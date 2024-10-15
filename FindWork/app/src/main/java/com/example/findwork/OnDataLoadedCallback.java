f36f565397b6b508ac2d9fb10d8425ccc3f1b1ce
pangansr <phamthanhliem011203@gmail.com>
2024-10-15 22:33:10 +0700

    v1

--- FindWork/app/src/main/java/com/example/findwork/OnDataLoadedCallback.java
+++
@@ -1,9 +0,0 @@
-package com.example.findwork;
-
-import com.example.findwork.Job.JobDataAapi;
-
-import java.util.List;
-
-public interface OnDataLoadedCallback {
-    void onDataLoaded(List<JobDataAapi.Job> jobList);
-}
--- FindWork/app/src/main/java/com/example/findwork/OnUriRetrievedListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.findwork;
-
-import android.net.Uri;
-
-public interface OnUriRetrievedListener {
-    void onUriRetrieved(Uri uri);
-}
