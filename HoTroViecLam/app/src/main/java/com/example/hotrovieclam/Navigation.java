9d645c638959da2e74a848caee7c1601a777ff81
ThaaanhTam <thanhtamm733@gmail.com>
2024-10-19 22:26:11 +0700

    Chua xong

---
+++ HoTroViecLam/app/src/main/java/com/example/hotrovieclam/Navigation.java
@@ -0,0 +1,58 @@
+package com.example.hotrovieclam;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.hotrovieclam.databinding.ActivityMainBinding;
+import com.example.hotrovieclam.databinding.NavigationBinding;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class Navigation extends AppCompatActivity {
+
+    private NavigationBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        binding = NavigationBinding.inflate(getLayoutInflater());
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+//        khởi chạy màn hình home đầu tiên
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new Home())
+                    .commit();
+        }
+        binding.navButtom.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                khuyến khích dùng if else dùng swith case sex bị lỗi R.id......
+                Fragment selectedFragment = null;
+                int id = item.getItemId();
+                if (id == R.id.home) {
+                    selectedFragment = new Home();
+                } else if (id == R.id.message) {
+                    selectedFragment = new Save_job();
+                }
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+}
