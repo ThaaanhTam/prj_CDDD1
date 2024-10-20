95cddf8b3f08a965de64ae0dcdadbec65a680313
NamTran181124 <namtran241811@gmail.com>
2024-10-20 14:12:34 +0700

    test

--- HoTroViecLam/app/src/main/java/com/example/hotrovieclam/Navigation.java
+++ HoTroViecLam/app/src/main/java/com/example/hotrovieclam/Navigation.java
@@ -5,13 +5,13 @@ import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
-import com.example.hotrovieclam.databinding.ActivityMainBinding;
+import com.example.hotrovieclam.Fragment.Home;
+import com.example.hotrovieclam.Fragment.Save_job;
 import com.example.hotrovieclam.databinding.NavigationBinding;
 import com.google.android.material.navigation.NavigationBarView;
 
