708e5f6ae03b0b2b3f7f41d5da0aeff9a5babf1d
pangansr <phamthanhliem011203@gmail.com>
2024-10-23 01:07:31 +0700

    fix: register_employer

--- HoTroViecLam/app/src/main/java/com/example/hotrovieclam/Buoc/RegisterEmployer.java
+++ HoTroViecLam/app/src/main/java/com/example/hotrovieclam/Buoc/RegisterEmployer.java
@@ -1,16 +1,26 @@
 package com.example.hotrovieclam.Buoc;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
 
-import com.example.hotrovieclam.databinding.RegisterEmployerBinding;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.FirebaseFirestore;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.hotrovieclam.R;
 
 public class RegisterEmployer extends AppCompatActivity {
 
-    private RegisterEmployerBinding binding;
-    private FirebaseFirestore db;
-    private FirebaseAuth mAuth;
-
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register_employer);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
 }
