9d4d99dd9bd3e8f14ab5b8199d700b5dbdf16bd6
pangansr <phamthanhliem011203@gmail.com>
2024-10-27 19:03:56 +0700

    additional Website.java

--- HoTroViecLam/app/src/main/java/com/example/hotrovieclam/Connect/Website.java
+++ HoTroViecLam/app/src/main/java/com/example/hotrovieclam/Connect/Website.java
@@ -22,7 +22,7 @@ import retrofit2.http.Url;
 public class Website {
     private static final String TAG = "TopCVScraper";
     private static final String URL = "https://123job.vn/tuyen-dung";// 2 luồng cho việc tải
-    private static final String URL2 = "https://careerviet.vn/viec-lam-noi-bat-trong-tuan-a30?utm_source=coccoc_context&utm_medium=CPC&utm_campaign=CCSP_DoiThu&utm_term=careerlink&utm_content=17899228&md=_05re50vdaa*Cr3MhoOtvxABYZwMXtuOsxfZFilEtJDJn3xwTnX7mmc7E0GaMWVsvJdMyZCfzeRDu4sW0R7W9v2sT3FwZLNexet*6rFFuaWu9ym9bQAjRbia8uVyC4at8QNR8.&ctm_event_id=2504129134";
+    private static final String URL2 = "https://careerviet.vn/viec-lam-noi-bat-trong-tuan-a3";
 
     public List<Job> loadWebsitesConcurrently() {
         List<Job> newJobList = new ArrayList<>();
@@ -39,54 +39,40 @@ public class Website {
 
 
                 Elements jobElements123job = document123job.select("div.job__list-item");
-                Elements jobElementsCareeviet = documentCareeviet.select("div.figure");
+                Elements jobElementsCareeviet = documentCareeviet.select("div.job-item");
 
                 for (Element jobElement : jobElementsCareeviet) {
-                    String id = jobElement.select("div.job__list-item").attr("data-id");
-                    Element jobTitleElement = jobElement.selectFirst(".lazy-img");
-                    String jobUrll = jobTitleElement.attr("src");
+                    String id = jobElement.attr("id");
+
+                    String image = jobElement.select("img.lazy-img").attr("data-src");
+                    String title = jobElement.select(".job_link").text();
+                    String salary = jobElement.select(".salary").text();
 
 
-                    String jobTitlee = jobTitleElement.attr("alt");
-                    Log.d("jobTitlee:  " , jobTitlee);
-                    Log.d("jobUrll:  " , jobUrll);
+                    Document getDescription = Jsoup.connect(jobElement.select(".job_link").attr("href"))
+                            .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+                            .timeout(90000)  // Tăng timeout lên 30 giây
+                            .get();
 
-//                    Document document2 = Jsoup.connect(jobUrl)
-//                            .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-//                            .timeout(90000)  // Tăng timeout lên 30 giây
-//                            .get();
-//
-//                    String description =  document2.select(".content-group__content").text();
-//                    String imgUrl = document2.select(".company-logo").attr("src");
-//                    //  imgUrl = imgUrl.replace("this.src='", "").replace("'", "");
-//                    if (imgUrl == null || imgUrl.isEmpty()) {
-//
-//                        imgUrl =  "https://123job.vn/images/no_company.png";
-//
-//                    }
-//
-//                    // Lấy tên công ty
-//                    String companyName = jobElement.selectFirst("div.job__list-item-company span").text();
-//
-//                    // Lấy địa điểm làm việc
-//                    String jobLocation = jobElement.selectFirst("div.address").text();
-//
-//                    // Lấy mức lương
-//                    String jobSalary = jobElement.selectFirst("div.salary").text();
-//
-//                    Job job = new Job();
-//                    job.setAvatar(imgUrl);
-//                    job.setId(id);
-//                    job.setTitle(jobTitle);
-//                    job.setDescription(description);
-//                    job.setLocation(jobLocation);
-//                    job.setAgreement(jobSalary);
-//                    newJobList.add(job);
+                    String description = getDescription.select("div.detail-row").text();
+                    String location = getDescription.select("div.place-name").text();
+//                    Log.d("salary:  " , salary);
+//                    Log.d("description1:  " , description);
+//                    Log.d("location:  " , location);
+//                    Log.d("id1:  " , id);
+//                    Log.d("image1:  " , image);
+//                    Log.d("title1:  " , title);
 
-                    // Lấy mô tả ngắn
-                    Log.d("im", jobUrll);
-                    Log.d("tt", jobTitlee);
-
+//
+                    Job job = new Job();
+                    job.setAvatar(image);
+                    job.setId(id);
+                    job.setTitle(title);
+                    job.setDescription(description);
+                    job.setLocation(location);
+                    job.setAgreement(salary);
+                    job.setSourceId(2);
+                    newJobList.add(job);
                 }
 
 
@@ -137,14 +123,14 @@ public class Website {
 
 
                     // In ra thông tin
-                    Log.d("Job Title: " , jobTitle);
-                    Log.d("Job URL: " , jobUrl);
-                    Log.d("Company Name: " , companyName);
-                    Log.d("Location: " , jobLocation);
-                    Log.d("Salary: " , jobSalary);
-                    Log.d("imgUrl: " , imgUrl);
-                    Log.d("description: " , description);
-                    Log.d("id: " , id);
+//                    Log.d("Job Title: " , jobTitle);
+//                    Log.d("Job URL: " , jobUrl);
+//                    Log.d("Company Name: " , companyName);
+//                    Log.d("Location: " , jobLocation);
+//                    Log.d("Salary: " , jobSalary);
+//                    Log.d("imgUrl: " , imgUrl);
+//                    Log.d("description: " , description);
+//                    Log.d("id: " , id);
                 }
 
             } catch (IOException e) {
