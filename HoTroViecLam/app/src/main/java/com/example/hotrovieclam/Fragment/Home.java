7d86d752682230431f9679bd78a11a7900807a56
ThaaanhTam <thanhtamm733@gmail.com>
2024-10-29 23:10:00 +0700

    a

--- HoTroViecLam/app/src/main/java/com/example/hotrovieclam/Fragment/Home.java
+++ HoTroViecLam/app/src/main/java/com/example/hotrovieclam/Fragment/Home.java
@@ -16,8 +16,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
