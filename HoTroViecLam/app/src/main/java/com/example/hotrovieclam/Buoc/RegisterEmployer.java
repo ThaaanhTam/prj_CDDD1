9399d115d5cc0cb3631f24af1cf596ca94514bd0
ThaaanhTam <thanhtamm733@gmail.com>
2024-11-06 07:38:57 +0700

    fix listjod

--- HoTroViecLam/app/src/main/java/com/example/hotrovieclam/Buoc/RegisterEmployer.java
+++ HoTroViecLam/app/src/main/java/com/example/hotrovieclam/Buoc/RegisterEmployer.java
@@ -20,6 +20,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
