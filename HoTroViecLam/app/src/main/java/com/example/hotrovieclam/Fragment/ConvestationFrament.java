61799b27e3a9d127bcab5472a5f247b05718756f
pangansr <phamthanhliem011203@gmail.com>
2024-11-25 09:22:08 +0700

    fix

--- HoTroViecLam/app/src/main/java/com/example/hotrovieclam/Fragment/ConvestationFrament.java
+++ HoTroViecLam/app/src/main/java/com/example/hotrovieclam/Fragment/ConvestationFrament.java
@@ -49,6 +49,7 @@ public class ConvestationFrament extends Fragment {
         binding.recyclerViewConversations.setAdapter(adapter);
 if(currentUserId!=null){
     fetchJobsFromFirestore();
+    Log.d("bbbb", "onCreateView: ");
 }
         return binding.getRoot();
     }
@@ -80,10 +81,20 @@ if(currentUserId!=null){
                                         }
 
                                         if (userSnapshot != null && userSnapshot.exists()) {
-                                            String name = userSnapshot.getString("name");
-                                            String avatar = userSnapshot.getString("avatar");
+                                            String name;
+                                            if(userSnapshot.getString("name")!=null) {
+                                                 name = userSnapshot.getString("name");
+                                            } else {
+                                                name = " ";
+                                            }
+                                            String avatar;
+                                            if(userSnapshot.getString("avatar")!=null){
+                                                avatar = userSnapshot.getString("avatar");
+                                            } else {
+                                                avatar = " ";
+                                            }
 
-                                            // Lắng nghe thay đổi tin nhắn cuối cùng
+
                                             db.collection("Message")
                                                     .document(messageID)
                                                     .collection("messages")
@@ -99,7 +110,7 @@ if(currentUserId!=null){
                                                             DocumentSnapshot lastMessageDoc = messageSnapshot.getDocuments().get(0);
                                                             String lastMessageContent = lastMessageDoc.getString("content");
                                                             long timestamp = lastMessageDoc.getLong("sent_at");
-                                                            
+
                                                             Date date = new Date(timestamp);
 
                                                             // Định dạng ngày giờ thành chuỗi
@@ -110,6 +121,10 @@ if(currentUserId!=null){
                                                             Log.d("FormattedDate", "Thời gian định dạng: " + formattedDate);
                                                             updateConversationList(
                                                                     documentId, messageID, status, name, avatar, lastMessageContent,formattedDate
+                                                            );
+                                                        }else {
+                                                            updateConversationList(
+                                                                    documentId, messageID, status, name, avatar, " "," "
                                                             );
                                                         }
                                                     });
@@ -132,7 +147,6 @@ if(currentUserId!=null){
         for (int i = 0; i < conversationList.size(); i++) {
             ListMess listMess = conversationList.get(i);
             if (listMess.getReicever_id().equals(documentId)) {
-                // Cập nhật hội thoại đã tồn tại
                 listMess.setMessID(messageID);
                 listMess.setStatus(status);
                 listMess.setName(name);
@@ -147,8 +161,6 @@ if(currentUserId!=null){
                 break;
             }
         }
-
-        // Nếu chưa tồn tại, thêm mới vào danh sách
         if (!isUpdated) {
             ListMess newListMess = new ListMess();
             newListMess.setReicever_id(documentId);
